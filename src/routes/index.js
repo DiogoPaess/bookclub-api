8492341799ef4a11d0c01be9c575e0fa851609a7
Diogo Paes <diogo.1@hotmail.com>
2023-11-09 15:12:46 +0000

    feat: generate book model and migration

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import authMiddleware from "../middlewares/auth";
 import UserController from "../controllers/user";
 import CategogoryController from "../controllers/category";
 import AuthorController from "../controllers/author";
+import BookController from "../controllers/book";
 
 const routes = new Router();
 
@@ -17,5 +18,7 @@ routes.get("/user", UserController.get);
 routes.get("/category", CategogoryController.getAll);
 routes.post("/author", AuthorController.create);
 routes.get("/author", AuthorController.getAll);
+routes.post("/book", BookController.create);
+routes.get("/book", BookController.findAll);
 
 export default routes;
